119c29c73cf9d9574fcfa409a905b3ed57f40931
Robin | RoboFlax <roboflax@gmail.com>
2018-03-01 15:55:19 +0100

    d

--- src/main/java/eu/roboflax/test/Pencil.java
+++ src/main/java/eu/roboflax/test/Pencil.java
@@ -6,7 +6,7 @@ package eu.roboflax.test;
 
 public class Pencil {
     
-    private String colorX;
+    public String colorX;
     
     public Pencil( String color ) {
         this.colorX = color;
